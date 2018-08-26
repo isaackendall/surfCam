371f1b3b21773933fb6741bbea23b9ff0f78e32e
Isaac Kendall <isaac@isaac-kendall.com>
2018-08-26 15:43:56 -0700

    Added light level reading

--- surfCam.ino
+++ surfCam.ino
@@ -1,49 +1,56 @@
 /*
 Test program to see if the TPL_5110 is working
 */
-# define DONEPIN                  5
-# define BATT_VOLTAGE_PIN         A0
-# define BATTERY_VOLTAGE          4
-# define ENABLE_PHONE_CHARGE_PIN  6
+# define DONEPIN          5
+# define BATT_VOLTAGE_PIN A0
+# define BATTERY_VOLTAGE  4
+# define LIGHT_LEVEL      A1
 
 void setup() {
   pinMode(DONEPIN, OUTPUT);
-  digitalWrite(DONEPIN, LOW); //Set the output of the pin to low keeping the TPL on. 
-  pinMode(ENABLE_PHONE_CHARGE_PIN, OUTPUT); //Pulls the enable pin of the powerBoost low, which disables 5v output
-  
+  digitalWrite(DONEPIN, LOW);
   Serial.begin(9600);
   Serial.println("Program starting");
+  delay(100);
 
 }
 
 void loop() {
-{
-  digitalWrite(ENABLE_PHONE_CHARGE_PIN, LOW);       // Turns the Power boost board off by setting the LB pin to High
-  delay(1000);                                      // waits for a second
-  Serial.println("turn off power boost");           // print turn off powerboost
-}
+  /*
+    // read the input on analog pin A0 (battery voltage):
+  int sensorValue = analogRead(BATT_VOLTAGE_PIN);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float batteryVoltage = sensorValue * (5.0 / 1023.0);
+  // print out the value you read:
+  Serial.print("batteryVoltage: "); // prints the value coming from the sensor on the screen
+  Serial.println(batteryVoltage); // puts it all on one line
+  delay(1500);
+*/
 
-  int sensorValue = analogRead(BATT_VOLTAGE_PIN);   //reads battery voltage 
+  int sensorValue = analogRead(BATT_VOLTAGE_PIN);
   delay(20);
   //batteryVoltage = analogRead(BATT_VOLTAGE_PIN);
   float batteryVoltage = (sensorValue * (0.00488281));
+  Serial.print("batteryVoltage: "); 
   Serial.println(batteryVoltage);
+  delay(100);
   
+  int light = analogRead(LIGHT_LEVEL);   //reads battery voltage 
+  delay(500);
+  //lightLevel = analogRead(BATT_VOLTAGE_PIN);
+  float lightLevel = (light * (1));
+  Serial.print("lightLevel: "); // prints the value coming from the sensor on the screen
+  Serial.println(lightLevel); // puts it all on one line
  
  if (batteryVoltage >= BATTERY_VOLTAGE) //checks to see if the voltage is OK.
 {
   Serial.println("voltage OK");
-  delay(1000);
-  digitalWrite(ENABLE_PHONE_CHARGE_PIN, HIGH);       // Turns the Power boost on
-  Serial.println("Power boost on!");
-  delay(1000);
+  delay(1500);
 } 
-
 if (batteryVoltage < BATTERY_VOLTAGE) //if voltage not OK, turn off circuit.
 {
   Serial.println("voltage not OK turing off circuit");
   delay(1500);
-  
   // toggle DONE so TPL knows to cut power!
   Serial.println("turnOffCircuit running and Switching Off");
   while (1) {
@@ -52,7 +59,27 @@ if (batteryVoltage < BATTERY_VOLTAGE) //if voltage not OK, turn off circuit.
     digitalWrite(DONEPIN, LOW);
     delay(1000);
   }
+  
+  
+
 }
   
 }
 
+/*
+Test program to see if the TPL_5110 is working
+
+//Triggers the done pin on the TPL5111
+void turnOffCircuit()
+{
+  // toggle DONE so TPL knows to cut power!
+  Serial.println("turnOffCircuit running and Switching Off");
+  while (1) {
+    digitalWrite(DONEPIN, HIGH);
+    delay(1);
+    digitalWrite(DONEPIN, LOW);
+    delay(1);
+  }
+ 
+}
+*/
