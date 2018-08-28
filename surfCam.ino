1119cfd78b41bfb157d13dc37d75de355f88ea90
Isaac Kendall <isaac@isaac-kendall.com>
2018-08-27 21:04:00 -0700

    If else code working!

--- surfCam.ino
+++ surfCam.ino
@@ -1,85 +1,63 @@
-/*
-Test program to see if the TPL_5110 is working
-*/
-# define DONEPIN          5
-# define BATT_VOLTAGE_PIN A0
-# define BATTERY_VOLTAGE  4
-# define LIGHT_LEVEL      A1
+
+# define LIGHT                      A1
+# define HIGH_LIGHT                 800
+# define MEDIUM_LIGHT               500
+# define LOW_LIGHT                  250
+# define BATT_VOLTAGE_PIN           A0
+# define DONEPIN                    5
+# define BATT_VOLT                  3.2
+#define  ENABLE_PHONE_CHARGE_PIN    4
+
+int val=0; //used for storing the value input from the sensor
+int ledVal = 0; //variable that control the LED output
+int light = analogRead(LIGHT);   //reads light level
 
 void setup() {
+  Serial.begin(9600); //for setting output to the monitor
   pinMode(DONEPIN, OUTPUT);
   digitalWrite(DONEPIN, LOW);
-  Serial.begin(9600);
-  Serial.println("Program starting");
-  delay(100);
-
 }
 
 void loop() {
-  /*
-    // read the input on analog pin A0 (battery voltage):
-  int sensorValue = analogRead(BATT_VOLTAGE_PIN);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float batteryVoltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.print("batteryVoltage: "); // prints the value coming from the sensor on the screen
-  Serial.println(batteryVoltage); // puts it all on one line
-  delay(1500);
-*/
-
-  int sensorValue = analogRead(BATT_VOLTAGE_PIN);
-  delay(20);
-  //batteryVoltage = analogRead(BATT_VOLTAGE_PIN);
-  float batteryVoltage = (sensorValue * (0.00488281));
-  Serial.print("batteryVoltage: "); 
-  Serial.println(batteryVoltage);
+  
+  int voltage = analogRead(BATT_VOLTAGE_PIN);
+  float batteryVoltage = (voltage * (0.00488281)); //voltage is 5/1024 
+  Serial.print("batteryVoltage: "); // prints the battery voltage
+  Serial.println(batteryVoltage); // put it all on one line
   delay(100);
   
-  int light = analogRead(LIGHT_LEVEL);   //reads battery voltage 
-  delay(500);
-  //lightLevel = analogRead(BATT_VOLTAGE_PIN);
+  int light = analogRead(LIGHT);   //reads light level 
   float lightLevel = (light * (1));
-  Serial.print("lightLevel: "); // prints the value coming from the sensor on the screen
+  Serial.print("lightLevel: "); // prints light level
   Serial.println(lightLevel); // puts it all on one line
- 
- if (batteryVoltage >= BATTERY_VOLTAGE) //checks to see if the voltage is OK.
-{
-  Serial.println("voltage OK");
-  delay(1500);
-} 
-if (batteryVoltage < BATTERY_VOLTAGE) //if voltage not OK, turn off circuit.
-{
-  Serial.println("voltage not OK turing off circuit");
-  delay(1500);
-  // toggle DONE so TPL knows to cut power!
-  Serial.println("turnOffCircuit running and Switching Off");
-  while (1) {
-    digitalWrite(DONEPIN, HIGH);
-    delay(1);
-    digitalWrite(DONEPIN, LOW);
-    delay(1000);
-  }
+  delay(500);
   
-  
-
-}
-  
+if((light > HIGH_LIGHT), (batteryVoltage > BATT_VOLT)){ //checks light level and battery voltage
+  Serial.println("voltage and light OK");
+  delay(1000);
 }
 
-/*
-Test program to see if the TPL_5110 is working
-
-//Triggers the done pin on the TPL5111
-void turnOffCircuit()
-{
-  // toggle DONE so TPL knows to cut power!
-  Serial.println("turnOffCircuit running and Switching Off");
-  while (1) {
-    digitalWrite(DONEPIN, HIGH);
-    delay(1);
-    digitalWrite(DONEPIN, LOW);
-    delay(1);
-  }
- 
+else if((light < HIGH_LIGHT), (batteryVoltage > BATT_VOLT)){
+//checks light level and battery voltage
+  Serial.println("voltage OK, light not");
+  delay(1000);
 }
-*/
+
+else if((light < LOW_LIGHT), (batteryVoltage > BATT_VOLT)){
+//checks light level and battery voltage
+  Serial.println("voltage OK, light level low");
+  delay(1000);
+}
+
+else if((light > LOW_LIGHT), (batteryVoltage > BATT_VOLT)){
+//checks light level and battery voltage
+  Serial.println("voltage OK, light level above low");
+  delay(1000);
+}
+
+else{
+//any input above 200 will get the maximum output
+Serial.println("no items matched"); // test to drop out. 
+}
+delay(100);
+}
